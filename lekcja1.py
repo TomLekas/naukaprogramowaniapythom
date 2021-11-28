403fe7d71d0e72553226e328d460228468acf29b
TomLekas <tomeklekas.krakow@gmail.com>
2021-11-28 17:12:39 +0100

    add
    aad2 in 28.11.2021

---
+++ lekcja1.py
@@ -0,0 +1,7 @@
+1 #lekcja pythona wyswietlanie liczb i tekstu w phytonie
+print("0023")
+# program nie wyswietli 0023 tylko 23
+print("hello word")
+print("Mój pierwszy program")
+print("5")
+print(5)
